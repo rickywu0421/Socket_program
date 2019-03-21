f26c490c83e42cc6c204d791b5bed38e7ef38653
rickywu0421 <rickywu0421@gmail.com>
2019-03-22 01:40:40 +0800

    almost finish

--- lab1_file_transfer.c
+++ lab1_file_transfer.c
@@ -10,10 +10,7 @@
 #include <time.h>
 
 #define MAX_SIZE 64
-#define EACH_TIME 1
 #define PIECE_NUM 20
-#define EACH_PERCENT 5
-
 
 void error(const char *msg);
 void tcp();
@@ -23,19 +20,12 @@ void udp();
 void udp_server(int sock);
 void udp_client(int sock);
 
+
 int sockfd,portno;
 char protocol[MAX_SIZE],send_or_recv[MAX_SIZE],serv_ip[MAX_SIZE],f_name[MAX_SIZE];
-char buffer[EACH_TIME];
-FILE *fp;
-size_t f_size;
-time_t timep;
-struct sockaddr_in serv_addr;
-struct tm *p;
-
-
 
 int main(int argc,char *argv[])
-{
+{   
     strcpy(protocol,argv[1]);
     strcpy(send_or_recv,argv[2]);
     strcpy(serv_ip,argv[3]);
@@ -55,7 +45,6 @@ int main(int argc,char *argv[])
         fprintf(stderr,"Please enter protocol in the second argument\n");
         exit(1);
     }
-
     return 0;
 }
 void tcp()
@@ -75,9 +64,13 @@ void tcp()
 void tcp_server(int sock)
 {   
     int newsock;
-    int count,piece,how_many_piece,percent;
+    int piece,count,left;
+    FILE *fp;
+    size_t f_size;
+    time_t timep;
     socklen_t clilen;
-    struct sockaddr_in cli_addr;
+    struct sockaddr_in serv_addr,cli_addr;
+    struct tm *p;
 
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -103,68 +96,77 @@ void tcp_server(int sock)
 
     if(send(newsock,&f_name,sizeof(f_name),0) < 0)
         error("error on sending filename");
+    
+    char buffer[piece];
 
     count = 0;
-    how_many_piece = 0;
-    percent = 0;
-    while(!feof(fp)){
-        count++;
-        fread(buffer,EACH_TIME,EACH_TIME,fp);
-        send(newsock,buffer,EACH_TIME,0);
-        memset(buffer,0,sizeof(buffer));
-        if(count == piece * (how_many_piece + 1)){   
+    left = f_size - 20 * piece;
+    if(f_size < PIECE_NUM){
+        fread(buffer,f_size,1,fp);
+        send(sock,buffer,f_size,0);
+    }
+    else{
+        while(1){
+            if(++count == 21)
+                break;
+            fread(buffer,piece,1,fp);
+            if(send(sock,buffer,piece,0) < 0)
+                error("error on sending file");
+            memset(buffer,0,sizeof(buffer));
+                
             time(&timep);  
             p = localtime(&timep);
-            percent += EACH_PERCENT;
-        
-            printf("%d%s  ",percent,"%");
+            printf("%d%s  ",count * 5,"%");
             printf("%d/%d/%d  ",(1900+p->tm_year),(1+p->tm_mon),(p->tm_mday));
             printf("%d:%d:%d\n",(p->tm_hour),(p->tm_min),(p->tm_sec));
-            
-            how_many_piece++;
         }
-    }
+        fread(buffer,left,1,fp);
+        send(sock,buffer,left,0);
+    }    
     close(newsock);
+    printf("done!\n");
+
     return;
 }
 void tcp_client(int sock)
 {   
-    int count,piece,how_many_piece,percent;
-    char output[MAX_SIZE] ="output_";
-    
+    int piece,count,left;
+    FILE *fp;
+    size_t f_size;
+    struct sockaddr_in serv_addr;
+
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
     serv_addr.sin_port = htons(portno);
     if(connect(sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("error on connecting");
+    if(recv(sock,&f_size,sizeof(f_size),0) < 0)
+        error("error on receving file size");
+    if(recv(sock,&f_name,sizeof(f_name),0) < 0)
+        error("error on receving file name");
     
-    recv(sock,&f_size,sizeof(f_size),0);
-    piece = f_size / PIECE_NUM;
-    
-    recv(sock,&f_name,sizeof(f_name),0);
+    char output[MAX_SIZE] = "output_";
     strcat(output,f_name);
-    
     fp = fopen(output,"wb");
-
+    piece = f_size / PIECE_NUM;
+    left = f_size - 20 * piece;
+    char buffer[piece];
     count = 0;
-    how_many_piece = 0;
-    percent = 0; 
-    while(recv(sock,buffer,EACH_TIME,0) != 0){
-        count++;
-        fwrite(buffer,EACH_TIME,EACH_TIME,fp);
-        memset(buffer,0,sizeof(buffer));
-        if(count == piece * (how_many_piece + 1)){
-            time(&timep);  
-            p = localtime(&timep);
-            percent += EACH_PERCENT;
-
-            printf("%d%s  ",percent,"%");
-            printf("%d/%d/%d  ",(1900+p->tm_year),(1+p->tm_mon),(p->tm_mday));
-            printf("%d:%d:%d\n",(p->tm_hour),(p->tm_min),(p->tm_sec));
-            how_many_piece++;
+    if(f_size < PIECE_NUM){
+        recv(sock,buffer,f_size,0);
+        fwrite(buffer,f_size,1,fp);
+    }
+    else{
+        while(1){
+            if(++count == 21)
+                break;
+            recv(sock,buffer,piece,0);
+            fwrite(buffer,piece,1,fp);
+            memset(buffer,0,sizeof(buffer));
         }
-
+        recv(sock,buffer,left,0);
+        fwrite(buffer,left,1,fp);
     }
     return;
 }
@@ -183,105 +185,127 @@ void udp()
 void udp_server(int sock)
 {
     int get_request,cli_finish;
-    int count,piece,how_many_piece,percent;
-    socklen_t serv_len;
-    
+    int piece,count,left;
+    FILE *fp;
+    size_t f_size;
+    time_t timep;
+    socklen_t cli_len;
+    struct sockaddr_in serv_addr,cli_addr;
+    struct tm *p;
+
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-    serv_len = sizeof(serv_addr);
-    if(bind(sock,(struct sockaddr *) &serv_addr,serv_len) < 0)
-       error("error on binding");
-    
+    if(bind(sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("error on binding");
     fp = fopen(f_name,"rb");
     fseek(fp,0,SEEK_END);
     f_size = ftell(fp);   //get file size
     piece = f_size / PIECE_NUM; 
     fseek(fp,0,SEEK_SET);
     
-    if(sendto(sock,&f_size,sizeof(f_size),0,(struct sockaddr *)&serv_addr,serv_len) < 0)
-        error("error on sending filesize");
+    cli_len = sizeof(cli_addr);
+    
+    while(1){
+        if(recvfrom(sock,&get_request,sizeof(get_request),0,(struct sockaddr *)&cli_addr,&cli_len) > 0)
+            break;
+    }
+    
+    if(sendto(sock,&f_name,sizeof(f_name),0,(struct sockaddr *)&cli_addr,cli_len) < 0)
+        error("error on sending file name");
+    if(sendto(sock,&f_size,sizeof(f_size),0,(struct sockaddr *)&cli_addr,cli_len) < 0)
+        error("error on sending file size");
+    
+    char buffer[piece];
 
     count = 0;
-    how_many_piece = 0;
-    percent = 0;
-    while(!feof(fp)){
-        count++;
-        fread(buffer,EACH_TIME,EACH_TIME,fp);
-        if(sendto(sock,buffer,EACH_TIME,0,(struct sockaddr *)&serv_addr,serv_len) < 0)
-            error("error on sending file");
-        memset(buffer,0,sizeof(buffer));
-        
-        if(count == piece * (how_many_piece + 1)){
-            percent += EACH_PERCENT;
+    left = f_size - 20* piece;
+    if(f_size < PIECE_NUM){
+        fread(buffer,f_size,1,fp);
+        sendto(sock,buffer,f_size,0,(struct sockaddr *)&cli_addr,cli_len);
+    }
+    else{
+        while(1){
+            if(++count == 21)
+                break;
+            fread(buffer,piece,1,fp);
+            if(sendto(sock,buffer,piece,0,(struct sockaddr *)&cli_addr,cli_len) < 0)
+                error("error on sending file");
+            memset(buffer,0,sizeof(buffer));
+                
             time(&timep);  
             p = localtime(&timep);
-            printf("%d%s  ",percent,"%");
+            printf("%d%s  ",count * 5,"%");
             printf("%d/%d/%d  ",(1900+p->tm_year),(1+p->tm_mon),(p->tm_mday));
             printf("%d:%d:%d\n",(p->tm_hour),(p->tm_min),(p->tm_sec));
-            how_many_piece++;
         }
-    }
-    while(1){
-        if(recvfrom(sock,&cli_finish,sizeof(cli_finish),0,(struct sockaddr *)&serv_addr,&serv_len) > 0)
-            return;
-    }
+        fread(buffer,left,1,fp);
+        sendto(sock,buffer,left,0,(struct sockaddr *)&cli_addr,cli_len);
+    }   
+    recvfrom(sock,&cli_finish,sizeof(cli_finish),0,(struct sockaddr *)&cli_addr,&cli_len);
+
+    printf("done!\n");
+    
+    return;
 }
 void udp_client(int sock)
 {
     int request,finish;
-    int count,piece,how_many_piece,percent;
-    char output[MAX_SIZE] = "output_";
+    int piece,count,left;
+    FILE *fp;
+    size_t f_size;
     socklen_t serv_len;
-    struct sockaddr_in my_addr;
+    struct sockaddr_in serv_addr,my_addr;
 
-    strcat(output,f_name);
-    
     memset(&my_addr,0,sizeof(my_addr));
     my_addr.sin_family = AF_INET;
     my_addr.sin_addr.s_addr = INADDR_ANY;
     my_addr.sin_port = htons(0);
-   
+
     if(bind(sock,(struct sockaddr *) &my_addr,sizeof(my_addr)) < 0)
         error("error on binding");
-    
+
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
     serv_addr.sin_port = htons(portno);
+
+    serv_len = sizeof(serv_addr);
+    if(sendto(sock,&request,sizeof(request),0,(struct sockaddr *) &serv_addr,serv_len) < 0)
+        error("error on sending request");
     
-    while(1){
-        if(recvfrom(sock,&f_size,sizeof(f_size),0,(struct sockaddr *) &serv_addr,&serv_len) > 0)
-            break;
-    }
+    if(recvfrom(sock,&f_name,sizeof(f_name),0,(struct sockaddr *) &serv_addr,&serv_len) < 0)
+        error("error on receiving file name");
+    if(recvfrom(sock,&f_size,sizeof(f_size),0,(struct sockaddr *) &serv_addr,&serv_len) < 0)
+        error("error on receiving file size");
     
+    char output[MAX_SIZE] = "output_"; 
+    strcat(output,f_name);
+    fp = fopen(output,"wb");
     piece = f_size / PIECE_NUM;
-    FILE *fp = fopen(output,"wb");
-    
+    left = f_size - 20 * piece;
+    char buffer[piece];
     count = 0;
-    how_many_piece = 0;
-    percent = 0;
-    while(recvfrom(sock,buffer,EACH_TIME,0,(struct sockaddr *) &serv_addr,&serv_len) != 0){
-        count++;
-        fwrite(buffer,EACH_TIME,EACH_TIME,fp);
-        memset(buffer,0,sizeof(buffer));
-        if(count == piece * (how_many_piece + 1)){
-            percent += EACH_PERCENT;
-            time(&timep);  
-            p = localtime(&timep);
-            
-            printf("%d%s  ",percent,"%");
-            printf("%d/%d/%d  ",(1900+p->tm_year),(1+p->tm_mon),(p->tm_mday));
-            printf("%d:%d:%d\n",(p->tm_hour),(p->tm_min),(p->tm_sec));
-            how_many_piece++;
+    if(f_size < PIECE_NUM){
+        recvfrom(sock,buffer,f_size,0,(struct sockaddr *) &serv_addr,&serv_len);
+        fwrite(buffer,f_size,1,fp);
+    }
+    else{
+        while(1){
+            if(++count == 21)
+                break;
+            recvfrom(sock,buffer,piece,0,(struct sockaddr *) &serv_addr,&serv_len);
+            fwrite(buffer,piece,1,fp);
+            memset(buffer,0,sizeof(buffer));
         }
+        recvfrom(sock,buffer,left,0,(struct sockaddr *) &serv_addr,&serv_len);
+        fwrite(buffer,left,1,fp);
     }
-    while(1){
-        if(sendto(sock,&finish,sizeof(finish),0,(struct sockaddr *) &serv_addr,serv_len) > 0) 
-            return;
+    if(sendto(sock,&finish,sizeof(finish),0,(struct sockaddr *) &serv_addr,serv_len) < 0)
+        error("error on sending finish message");
         
-    }
+    return;
 }
 void error(const char *msg)
 {
